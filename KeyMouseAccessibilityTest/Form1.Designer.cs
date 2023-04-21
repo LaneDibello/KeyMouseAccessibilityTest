d411d2b464560a951f4eb021216e9ef0361feaaf
Lane Dibello <lane.dibello@gmail.com>
2023-04-21 12:01:58 -0500

    Cleanign up

--- KeyMouseAccessibilityTest/Form1.Designer.cs
+++ KeyMouseAccessibilityTest/Form1.Designer.cs
@@ -28,133 +28,252 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.mainControl = new System.Windows.Forms.Label();
-            this.cursorX = new System.Windows.Forms.Label();
-            this.cursorY = new System.Windows.Forms.Label();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.pressedDebug = new System.Windows.Forms.Label();
-            this.xydirLabel = new System.Windows.Forms.Label();
-            this.xymovLabel = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // mainControl
-            // 
-            this.mainControl.AutoSize = true;
-            this.mainControl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.mainControl.Location = new System.Drawing.Point(645, 9);
-            this.mainControl.Name = "mainControl";
-            this.mainControl.Size = new System.Drawing.Size(161, 64);
-            this.mainControl.TabIndex = 0;
-            this.mainControl.Text = "TEMPORARY\r\nPROTOTYPE";
-            this.mainControl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // cursorX
-            // 
-            this.cursorX.AutoSize = true;
-            this.cursorX.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.cursorX.Location = new System.Drawing.Point(552, 278);
-            this.cursorX.Name = "cursorX";
-            this.cursorX.Size = new System.Drawing.Size(110, 32);
-            this.cursorX.TabIndex = 2;
-            this.cursorX.Text = "cursor X";
-            this.cursorX.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // cursorY
-            // 
-            this.cursorY.AutoSize = true;
-            this.cursorY.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.cursorY.Location = new System.Drawing.Point(802, 278);
-            this.cursorY.Name = "cursorY";
-            this.cursorY.Size = new System.Drawing.Size(109, 32);
-            this.cursorY.TabIndex = 3;
-            this.cursorY.Text = "cursor Y";
-            this.cursorY.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            lUpLeft = new TextBox();
+            lUp = new TextBox();
+            lUpRight = new TextBox();
+            lRight = new TextBox();
+            lDownRight = new TextBox();
+            lDown = new TextBox();
+            lDownLeft = new TextBox();
+            lLeft = new TextBox();
+            lMouseDir = new Label();
+            lLeftClick = new TextBox();
+            lRightClick = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            panel1 = new Panel();
+            SuspendLayout();
             // 
             // timer1
             // 
-            this.timer1.Enabled = true;
-            this.timer1.Interval = 5;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            timer1.Enabled = true;
+            timer1.Interval = 5;
+            timer1.Tick += timer1_Tick;
             // 
-            // pressedDebug
+            // lUpLeft
             // 
-            this.pressedDebug.AutoSize = true;
-            this.pressedDebug.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.pressedDebug.Location = new System.Drawing.Point(680, 403);
-            this.pressedDebug.Name = "pressedDebug";
-            this.pressedDebug.Size = new System.Drawing.Size(102, 32);
-            this.pressedDebug.TabIndex = 4;
-            this.pressedDebug.Text = "Pressed";
-            this.pressedDebug.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            lUpLeft.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lUpLeft.Location = new Point(16, 16);
+            lUpLeft.Name = "lUpLeft";
+            lUpLeft.ReadOnly = true;
+            lUpLeft.Size = new Size(80, 26);
+            lUpLeft.TabIndex = 0;
+            lUpLeft.Text = "KEY";
+            lUpLeft.TextAlign = HorizontalAlignment.Center;
+            lUpLeft.Enter += keybindEnter;
+            lUpLeft.KeyDown += kebindPress;
+            lUpLeft.Leave += keybindLeave;
             // 
-            // xydirLabel
+            // lUp
             // 
-            this.xydirLabel.AutoSize = true;
-            this.xydirLabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.xydirLabel.Location = new System.Drawing.Point(680, 510);
-            this.xydirLabel.Name = "xydirLabel";
-            this.xydirLabel.Size = new System.Drawing.Size(46, 32);
-            this.xydirLabel.TabIndex = 5;
-            this.xydirLabel.Text = "dir";
-            this.xydirLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            lUp.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lUp.Location = new Point(120, 16);
+            lUp.Name = "lUp";
+            lUp.ReadOnly = true;
+            lUp.Size = new Size(80, 26);
+            lUp.TabIndex = 1;
+            lUp.Text = "KEY";
+            lUp.TextAlign = HorizontalAlignment.Center;
+            lUp.Enter += keybindEnter;
+            lUp.KeyDown += kebindPress;
+            lUp.Leave += keybindLeave;
             // 
-            // xymovLabel
+            // lUpRight
             // 
-            this.xymovLabel.AutoSize = true;
-            this.xymovLabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.xymovLabel.Location = new System.Drawing.Point(680, 574);
-            this.xymovLabel.Name = "xymovLabel";
-            this.xymovLabel.Size = new System.Drawing.Size(64, 32);
-            this.xymovLabel.TabIndex = 6;
-            this.xymovLabel.Text = "mov";
-            this.xymovLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            lUpRight.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lUpRight.Location = new Point(224, 16);
+            lUpRight.Name = "lUpRight";
+            lUpRight.ReadOnly = true;
+            lUpRight.Size = new Size(80, 26);
+            lUpRight.TabIndex = 2;
+            lUpRight.Text = "KEY";
+            lUpRight.TextAlign = HorizontalAlignment.Center;
+            lUpRight.Enter += keybindEnter;
+            lUpRight.KeyDown += kebindPress;
+            lUpRight.Leave += keybindLeave;
             // 
-            // textBox1
+            // lRight
             // 
-            this.textBox1.Location = new System.Drawing.Point(30, 36);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(437, 680);
-            this.textBox1.TabIndex = 7;
-            this.textBox1.Text = resources.GetString("textBox1.Text");
+            lRight.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lRight.Location = new Point(224, 64);
+            lRight.Name = "lRight";
+            lRight.ReadOnly = true;
+            lRight.Size = new Size(80, 26);
+            lRight.TabIndex = 4;
+            lRight.Text = "KEY";
+            lRight.TextAlign = HorizontalAlignment.Center;
+            lRight.Enter += keybindEnter;
+            lRight.KeyDown += kebindPress;
+            lRight.Leave += keybindLeave;
+            // 
+            // lDownRight
+            // 
+            lDownRight.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lDownRight.Location = new Point(224, 112);
+            lDownRight.Name = "lDownRight";
+            lDownRight.ReadOnly = true;
+            lDownRight.Size = new Size(80, 26);
+            lDownRight.TabIndex = 7;
+            lDownRight.Text = "KEY";
+            lDownRight.TextAlign = HorizontalAlignment.Center;
+            lDownRight.Enter += keybindEnter;
+            lDownRight.KeyDown += kebindPress;
+            lDownRight.Leave += keybindLeave;
+            // 
+            // lDown
+            // 
+            lDown.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lDown.Location = new Point(120, 112);
+            lDown.Name = "lDown";
+            lDown.ReadOnly = true;
+            lDown.Size = new Size(80, 26);
+            lDown.TabIndex = 6;
+            lDown.Text = "KEY";
+            lDown.TextAlign = HorizontalAlignment.Center;
+            lDown.Enter += keybindEnter;
+            lDown.KeyDown += kebindPress;
+            lDown.Leave += keybindLeave;
+            // 
+            // lDownLeft
+            // 
+            lDownLeft.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lDownLeft.Location = new Point(16, 112);
+            lDownLeft.Name = "lDownLeft";
+            lDownLeft.ReadOnly = true;
+            lDownLeft.Size = new Size(80, 26);
+            lDownLeft.TabIndex = 5;
+            lDownLeft.Text = "KEY";
+            lDownLeft.TextAlign = HorizontalAlignment.Center;
+            lDownLeft.Enter += keybindEnter;
+            lDownLeft.KeyDown += kebindPress;
+            lDownLeft.Leave += keybindLeave;
+            // 
+            // lLeft
+            // 
+            lLeft.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lLeft.Location = new Point(16, 64);
+            lLeft.Name = "lLeft";
+            lLeft.ReadOnly = true;
+            lLeft.Size = new Size(80, 26);
+            lLeft.TabIndex = 3;
+            lLeft.Text = "KEY";
+            lLeft.TextAlign = HorizontalAlignment.Center;
+            lLeft.Enter += keybindEnter;
+            lLeft.KeyDown += kebindPress;
+            lLeft.Leave += keybindLeave;
+            // 
+            // lMouseDir
+            // 
+            lMouseDir.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lMouseDir.Location = new Point(120, 56);
+            lMouseDir.Name = "lMouseDir";
+            lMouseDir.Size = new Size(80, 40);
+            lMouseDir.TabIndex = 8;
+            lMouseDir.Text = "Mouse\r\nDirections";
+            lMouseDir.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lLeftClick
+            // 
+            lLeftClick.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lLeftClick.Location = new Point(16, 168);
+            lLeftClick.Name = "lLeftClick";
+            lLeftClick.ReadOnly = true;
+            lLeftClick.Size = new Size(80, 26);
+            lLeftClick.TabIndex = 9;
+            lLeftClick.Text = "KEY";
+            lLeftClick.TextAlign = HorizontalAlignment.Center;
+            lLeftClick.Enter += keybindEnter;
+            lLeftClick.KeyDown += kebindPress;
+            lLeftClick.Leave += keybindLeave;
+            // 
+            // lRightClick
+            // 
+            lRightClick.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lRightClick.Location = new Point(16, 208);
+            lRightClick.Name = "lRightClick";
+            lRightClick.ReadOnly = true;
+            lRightClick.Size = new Size(80, 26);
+            lRightClick.TabIndex = 10;
+            lRightClick.Text = "KEY";
+            lRightClick.TextAlign = HorizontalAlignment.Center;
+            lRightClick.Enter += keybindEnter;
+            lRightClick.KeyDown += kebindPress;
+            lRightClick.Leave += keybindLeave;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(120, 168);
+            label2.Name = "label2";
+            label2.Size = new Size(184, 24);
+            label2.TabIndex = 12;
+            label2.Text = "Left Click";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(120, 208);
+            label1.Name = "label1";
+            label1.Size = new Size(184, 24);
+            label1.TabIndex = 13;
+            label1.Text = "Right Click";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(8, 248);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(96, 40);
+            panel1.TabIndex = 14;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1386, 826);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.xymovLabel);
-            this.Controls.Add(this.xydirLabel);
-            this.Controls.Add(this.pressedDebug);
-            this.Controls.Add(this.cursorY);
-            this.Controls.Add(this.cursorX);
-            this.Controls.Add(this.mainControl);
-            this.KeyPreview = true;
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Enter += new System.EventHandler(this.Form1_Enter);
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
-            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(321, 557);
+            Controls.Add(panel1);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(lRightClick);
+            Controls.Add(lLeftClick);
+            Controls.Add(lMouseDir);
+            Controls.Add(lLeft);
+            Controls.Add(lDownLeft);
+            Controls.Add(lDown);
+            Controls.Add(lDownRight);
+            Controls.Add(lRight);
+            Controls.Add(lUpRight);
+            Controls.Add(lUp);
+            Controls.Add(lUpLeft);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            KeyPreview = true;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Keyboard Mouse Movement";
+            KeyDown += Form1_KeyDown;
+            KeyUp += Form1_KeyUp;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-
-        private Label mainControl;
-        private Label cursorX;
-        private Label cursorY;
         private System.Windows.Forms.Timer timer1;
-        private Label pressedDebug;
-        private Label xydirLabel;
-        private Label xymovLabel;
-        private TextBox textBox1;
+        private TextBox lUpLeft;
+        private TextBox lUp;
+        private TextBox lUpRight;
+        private TextBox lRight;
+        private TextBox lDownRight;
+        private TextBox lDown;
+        private TextBox lDownLeft;
+        private TextBox lLeft;
+        private Label lMouseDir;
+        private TextBox lLeftClick;
+        private TextBox lRightClick;
+        private Label label2;
+        private Label label1;
+        private Panel panel1;
     }
 }
---
+++ KeyMouseAccessibilityTest/MouseOperations.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyMouseAccessibilityTest
+{
+    public class MouseOperations
+    {
+        //Below are the assembly references that attach to the mouse.
+        #region Assembly references
+        //Grab Set cursor Position from user32.dll
+        [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetCursorPos(int x, int y);
+
+        //Grab The Mouse Event Function from user32.dll
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+        private const int MOUSE_EVENT_MOVE = 0x0001;
+        private const int MOUSE_EVENT_LEFTDOWN = 0x0002;
+        private const int MOUSE_EVENT_LEFTUP = 0x0004;
+        private const int MOUSE_EVENT_RIGHTDOWN = 0x0008;
+        private const int MOUSE_EVENT_RIGHTUP = 0x0010;
+        private const int MOUSE_EVENT_MIDDLEDOWN = 0x0020;
+        private const int MOUSE_EVENT_MIDDLEUP = 0x0040;
+        private const int MOUSE_EVENT_ABSOLUTE = 0x8000;
+        #endregion
+
+        public int mouseSpeed = 2; //Mouse speed in arbitrary speed units
+        public double x_dir = 0; //X component of the mouse movemetn vector
+        public double y_dir = 0; //Y component of the mouse movement vector
+        public dirBitFlags curr_mov; //Current Movement diretcions
+
+        // Left clicks the mouse
+        public static void LeftDown()
+        {
+            mouse_event(MOUSE_EVENT_LEFTDOWN, Cursor.Position.X, Cursor.Position.Y, 0, 0);
+        }
+
+        public static void LeftUp()
+        {
+            mouse_event(MOUSE_EVENT_LEFTUP, Cursor.Position.X, Cursor.Position.Y, 0, 0);
+        }
+
+        public void adjMovAtomic(dirBitFlags f, bool set)
+        {
+            lock (this)
+            {
+                if (set) curr_mov |= f;
+                else curr_mov &= ~f;
+
+                int x_mag = (int)(
+                    ((int)(curr_mov & dirBitFlags.DOWN_RIGHT) >> 2) +
+                    ((int)(curr_mov & dirBitFlags.RIGHT) >> 4) +
+                    ((int)(curr_mov & dirBitFlags.UP_RIGHT) >> 7) -
+                    ((int)(curr_mov & dirBitFlags.DOWN_LEFT) >> 0) -
+                    ((int)(curr_mov & dirBitFlags.LEFT) >> 3) -
+                    ((int)(curr_mov & dirBitFlags.UP_LEFT) >> 5));
+
+                int y_mag = (int)(
+                    ((int)(curr_mov & dirBitFlags.DOWN_RIGHT) >> 2) +
+                    ((int)(curr_mov & dirBitFlags.DOWN) >> 1) +
+                    ((int)(curr_mov & dirBitFlags.DOWN_LEFT) >> 0) -
+                    ((int)(curr_mov & dirBitFlags.UP_RIGHT) >> 7) -
+                    ((int)(curr_mov & dirBitFlags.UP) >> 6) -
+                    ((int)(curr_mov & dirBitFlags.UP_LEFT) >> 5));
+
+                double mag = Math.Sqrt(x_mag * x_mag + y_mag * y_mag);
+                if (mag <= 0)
+                {
+                    x_dir = 0;
+                    y_dir = 0;
+                }
+                else
+                {
+                    x_dir = x_mag / mag;
+                    y_dir = y_mag / mag;
+                }
+            }
+        }
+    }
+}
